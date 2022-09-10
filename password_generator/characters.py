367afb96a22d114936bb517f5728649f4a453387
Kiwi <88976806+TH1RDKEYBLADE@users.noreply.github.com>
2022-09-10 00:28:00 -0400

    Create characters.py

---
+++ password_generator/characters.py
@@ -0,0 +1,12 @@
+# Made by TH1RDKEYBLADE
+# Settings.py
+
+string_upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+string_lower_case = string_upper_case.lower()
+string_special_char = "!@#$%^&*()-_=+/?.>,<;:\'\"|\\"
+string_numbers = "0123456789"
+
+list_upper_case = list(string_upper_case)
+list_lower_case = list(string_lower_case)
+list_special_char = list(string_special_char)
+list_numbers = list(string_numbers)
